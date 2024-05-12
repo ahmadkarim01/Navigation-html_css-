467adda76591b317896755c1151df92b621b7a24
Ahmad Karim <ahmadkarimarbab@gmail.com>
2024-05-12 23:57:07 +0500

    test3.js

---
+++ test3.js
@@ -0,0 +1,10 @@
+const product = {
+    name: "Book",
+    price: 250,
+    color: "red",
+    rating: 5,
+    offer: 6,
+    isAdd: ture       //this is for add to card button
+}
+
+console.log(product);
