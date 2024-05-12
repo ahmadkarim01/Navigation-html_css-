c1147fbf3382f0d161f3ee1e83e887125509eeae
Ahmad Karim <ahmadkarimarbab@gmail.com>
2024-05-12 23:51:30 +0500

    test2.js

---
+++ test2.js
@@ -0,0 +1,21 @@
+const instagram = {
+    name: "Ahmad Karim",
+    isFollow: true,
+    followers: 170,
+    following: 130,
+};
+
+console.log(profile);
+
+const youtube = {
+    name: "Ahmad karim",
+    isFollow: false,
+    subscribers: 89999990,
+    likes: 23444445,
+    posts: 324,
+    earning: 434934
+};
+
+console.log(youtube);
+
+let age = 20;
