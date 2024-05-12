27a8ea3ec084c36fa61bed773a830b3723aff08e
Ahmad Karim <ahmadkarimarbab@gmail.com>
2024-05-12 23:50:45 +0500

    test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+// console.log
+// "This is the simple code in javascript"
+
+
+/*firstName = Ahmad;
+console.log(firstName);*/
+
+
+let unFollow = "Facebook page";
+alert(unFollow);
+
+
+let userName = "ahmad";
+let userPassword = 123;
+let userID = 2020;
+let userPin = 3433;
